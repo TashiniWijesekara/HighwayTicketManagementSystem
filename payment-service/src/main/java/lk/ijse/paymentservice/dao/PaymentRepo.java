df5e6a0ed7b9701b84e8c1dd035644ba6b9f5ed2
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-07-04 00:53:41 +0530

    modify

--- payment-service/src/main/java/lk/ijse/paymentservice/dao/PaymentRepo.java
+++ payment-service/src/main/java/lk/ijse/paymentservice/dao/PaymentRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PaymentRepo extends JpaRepository {
+public interface PaymentRepo extends JpaRepository<PaymentEntity,String> {
     List<PaymentEntity> findByTicketId(String ticketId);
 
 }
